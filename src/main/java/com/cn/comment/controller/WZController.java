53c5cb4a8f6ee015c39be817613f6675e44040f3
pengdeman <872422345@qq.com>
2019-01-07 16:04:16 +0800

    TEST COMMIT

--- src/main/java/com/cn/comment/controller/WZController.java
+++ src/main/java/com/cn/comment/controller/WZController.java
@@ -11,9 +11,13 @@ import java.util.Map;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cn.comment.pojo.User;
 import com.cn.comment.pojo.Wz;
 import com.cn.comment.service.IWzService;
 import com.cn.comment.util.GetLatAndLngByBaiduUtil;
@@ -40,27 +44,32 @@ public class WZController {
     
     @RequestMapping("/wzinsert")
     public String  wzinsert(HttpServletRequest request, HttpServletResponse response, Model model) throws IOException{ 
+    	HttpSession session = request.getSession();
     	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    	User user= (User)session.getAttribute("login");
     	String lng = request.getParameter("lng");//经度
     	String lat = request.getParameter("lat");//纬度
     	String name = request.getParameter("name");//纬度
-    	
-    	String wz = GetLatAndLngByBaiduUtil.getCityFromLngAndlat(lat, lng);
-    	Wz wzb = new Wz();
-    	wzb.setAdr(wz);
-    	wzb.setLng(lng);
-    	wzb.setLat(lat);
-    	wzb.setCreTime(sdf.format(new Date()));
-    	wzb.setName(name);
-    	wzService.insert(wzb);
-    	
-    	Map<String, Object> dateMap = new HashMap<String, Object>();
-    	dateMap.put("wz", wz);
-    	response.setCharacterEncoding("utf-8");
-    	PrintWriter pw = response.getWriter();
-    	pw.print(JsonUtil.toJson(dateMap).toString());
-    	pw.flush();
-        return null;  
+    	if(user != null && user.getMail().equals(name)) {
+    		return null;  
+    	}else {
+    		String wz = GetLatAndLngByBaiduUtil.getCityFromLngAndlat(lat, lng);
+    		Wz wzb = new Wz();
+    		wzb.setAdr(wz);
+    		wzb.setLng(lng);
+    		wzb.setLat(lat);
+    		wzb.setCreTime(sdf.format(new Date()));
+    		wzb.setName(name);
+    		wzService.insert(wzb);
+    		
+    		Map<String, Object> dateMap = new HashMap<String, Object>();
+    		dateMap.put("wz", wz);
+    		response.setCharacterEncoding("utf-8");
+    		PrintWriter pw = response.getWriter();
+    		pw.print(JsonUtil.toJson(dateMap).toString());
+    		pw.flush();
+    		return null;  
+    	}
     }
     
     @RequestMapping("/getwzlist")
